e17201ba95ffa175af263381b1aa900cc121cb1c
Devon <devon6608@gmail.com>
2024-10-01 16:53:34 -0700

    Update BasicWheelsCode.java
    Removed un-needed imports

--- BasicWheelsCode.java
+++ BasicWheelsCode.java
@@ -4,11 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import java.util.List;
-import java.util.ArrayList;
-
 
 
 @TeleOp(name="BasicWheelsCode", group="2024Op")
