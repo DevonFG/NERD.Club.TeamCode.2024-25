89e85ed712b599dc77fa159d70c3a556553627be
rfleming01 <151577884+rfleming01@users.noreply.github.com>
2024-11-12 16:28:05 -0800

    Update BasicDriveCodeComplete.java

--- BasicDriveCodeComplete.java
+++ BasicDriveCodeComplete.java
@@ -5,3 +5,5 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+//import the hardware file//
+import org.firstinspires.ftc.teamcode.HardwareFile2024
