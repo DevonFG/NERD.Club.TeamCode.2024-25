9ee64b84c5982f197a298be6204608e8b131b7d2
rfleming01 <151577884+rfleming01@users.noreply.github.com>
2024-11-14 16:46:32 -0800

    Update leftSideBasicAutonomous.java
    Left autonomous no longer has repeat code, and is linked to the auto hardware.

--- leftSideBasicAutonomous.java
+++ leftSideBasicAutonomous.java
@@ -1,48 +1,7 @@
 import org.firstinspires.ftc.teamcode.HardwareFile2024;
-
+import org.firstinspires.ftc.teamcode.AutonomousHardware;
 @Autonomous(name="LeftSideBasicAutonomous", group="9044.NERD.")
 public class Autonomous extends LinearOpMode {
-  
-    // Declare OpMode members.
-    // private ElapsedTime runtime = new ElapsedTime();
-    
-  // only parameter is direction, give either string of f or b.    
-    public void forwardAndBackward(String direction) {
-      if (direction == "f") {
-        setMotorPower(leftFrontWheel,   1.0);
-        setMotorPower(leftBackWheel,    1.0);
-        setMotorPower(rightFrontWheel,  1.0);
-        setMotorPower(rightBackWheel,   1.0);
-      } else if (direction == "b") {
-        setMotorPower(leftFrontWheel,  -1.0);
-        setMotorPower(leftBackWheel,   -1.0);
-        setMotorPower(rightFrontWheel, -1.0);
-        setMotorPower(rightBackWheel,  -1.0);              
-      }
-    }
-
-    // only parameter is direction, give either string of r or l.
-    private void sideways (String direction) {
-      if (direction == "r") {
-        setMotorPower(leftFrontWheel,   1.0);
-        setMotorPower(leftBackWheel,   -1.0);
-        setMotorPower(rightFrontWheel, -1.0);
-        setMotorPower(rightBackWheel,   1.0);
-      } else if (direction == "l") {
-        setMotorPower(leftFrontWheel,  -1.0);
-        setMotorPower(leftBackWheel,    1.0);
-        setMotorPower(rightFrontWheel,  1.0);
-        setMotorPower(rightBackWheel,  -1.0);
-      }
-    }
-
-    // no parameters, just sets all motors to power 0
-     private void reset() {
-        setMotorPower(leftFrontWheel,   0.0);
-        setMotorPower(leftBackWheel,    0.0);
-        setMotorPower(rightFrontWheel,  0.0);
-        setMotorPower(rightBackWheel,   0.0);
-    }
 
     @Override
     public void runOpMode() {
@@ -57,7 +16,7 @@ public class Autonomous extends LinearOpMode {
           */
 
           forwardAndBackward("f");
-          sleep(2000);
+          sleep(1000);
           reset();
           sideways("r");
           sleep(1000);
