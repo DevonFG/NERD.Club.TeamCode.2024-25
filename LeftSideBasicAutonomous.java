131044a206a60d4a73448b4cb1e1ad3f3b347d63
rfleming01 <151577884+rfleming01@users.noreply.github.com>
2024-11-16 12:08:10 -0800

    Update LeftSideBasicAutonomous.java
    Fixed format from errors we say while debugging drive code

--- LeftSideBasicAutonomous.java
+++ LeftSideBasicAutonomous.java
@@ -1,20 +1,22 @@
-import org.firstinspires.ftc.teamcode.HardwareFile2024;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+// import org.firstinspires.ftc.teamcode.HardwareFile2024;
 import org.firstinspires.ftc.teamcode.AutonomousHardware;
+
 @Autonomous(name="LeftSideBasicAutonomous", group="9044.NERD.")
 public class Autonomous extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-      
+        
+        HardwareFile2024 robot = new HardwareFile2024(this);
+        robot.init();
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-      /*    
-          
-          */
-
+/*
           forwardAndBackward("f");
           sleep(1000);
           reset();
@@ -24,7 +26,7 @@ public class Autonomous extends LinearOpMode {
           forwardAndBackward("f");
           sleep(1000);
           reset();
-          
+          */
           
           }
         }
