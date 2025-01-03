09296fbb933d8f73d558cfcbe67a33ea50fa8357
rfleming01 <151577884+rfleming01@users.noreply.github.com>
2025-01-03 15:53:33 -0800

    Update RobotHardware.java
    pasting over changes from debugging

--- RobotHardware.java
+++ RobotHardware.java
@@ -1,21 +1,12 @@
-//Need to program limit switches to stop the brush
 
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import java.lang.reflect.Array;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.TouchSensor;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.lang.Thread;
 
 public class HardwareFile2024 {
     
@@ -28,39 +19,9 @@ public class HardwareFile2024 {
     public DcMotor leftBackWheel = null;
     public DcMotor rightFrontWheel = null;
     public DcMotor rightBackWheel = null;
-
-    public DcMotor leftFoot  = null; //Motors for the feet
-    public DcMotor rightFoot = null;
-    
-    public DcMotor screwLift = null; //Motors for the archimedes screw
-    public DcMotor screwTurn = null;
-
-    public CRServo highFinger = null; //Servos for the brush
-    public CRServo lowFinger  = null;
-    public CRServo leftSweepOutServo = null;
-    public CRServo rightSweepOutServo = null;
-
-    public TouchSensor brushExtended;
-    public TouchSensor brushRetracted;
-
-    public Servo screwDoor;
-
-    private double  BRUSH_SPEED           = 0.5; //Set betwwen 0.0 and 0.5
-    private double  REACH_SPEED           = 0.3;
-    private boolean brushMoving           = false;
-    private double  FULL_FEET_LIFT_SPEED  = 2000; // miliseconds, not tested or measured
-    private double  LOW_FEET_LIFT_SPEED   = 1000; // miliseconds, not tested or measured
-    private double  FULL_SCREW_LIFT_SPEED = 2000; // miliseconds, not tested or measured
-    private double  LOW_SCREW_LIFT_SPEED  = 1000; // miliseconds, not tested or measured
-
-    public double PANEL             = 2000;    // miliseconds
-    public double INCH              = 2000/24; // miliseconds
-
-    List<DcMotor> allMotors         = new ArrayList<>();
-    List<CRServo> allCRServos       = new ArrayList<>();
-    List<Servo>   allServos         = new ArrayList<>();
     
     // Define a constructor that allows the OpMode to pass a reference to itself.
+
 
     /**
      * Initialize all the robot's hardware.
@@ -74,7 +35,7 @@ public class HardwareFile2024 {
     }
      
     public void init()    {
-        
+
         leftFrontWheel  = myOpMode.hardwareMap.get(DcMotor.class, "leftFront");
         leftBackWheel   = myOpMode.hardwareMap.get(DcMotor.class, "leftBack");
         rightFrontWheel = myOpMode.hardwareMap.get(DcMotor.class, "rightFront");
@@ -85,37 +46,8 @@ public class HardwareFile2024 {
         rightFrontWheel.setDirection(DcMotor.Direction.FORWARD);
         rightBackWheel.setDirection(DcMotor.Direction.FORWARD);
 
-        leftFoot            = myOpMode.hardwareMap.get(DcMotor.class, "myLeftFoot");
-        rightFoot           = myOpMode.hardwareMap.get(DcMotor.class, "myRightFoot");
-
-        screwLift           = myOpMode.hardwareMap.get(DcMotor.class, "archimedes");
-        screwTurn           = myOpMode.hardwareMap.get(DcMotor.class, "brush");
-
-        highFinger          = myOpMode.hardwareMap.get(CRServo.class, "highArmFinger");
-        lowFinger           = myOpMode.hardwareMap.get(CRServo.class, "lowArmFinger");
-        leftSweepOutServo   = myOpMode.hardwareMap.get(CRServo.class, "leftSweepOut");
-        rightSweepOutServo  = myOpMode.hardwareMap.get(CRServo.class, "rightSweepOut");
-
-        //Need to put into config file
-        screwDoor = myOpMode.hardwareMap.get(Servo.class, "archimedesScrewDoor");
-        
-        allMotors.add(leftFrontWheel);
-        allMotors.add(leftBackWheel);
-        allMotors.add(rightFrontWheel);
-        allMotors.add(rightBackWheel);
-        allMotors.add(screwLift);
-        allMotors.add(screwTurn);
-        allMotors.add(leftFoot);
-        allMotors.add(rightFoot);
-
-        allCRServos.add(highFinger);
-        allCRServos.add(lowFinger);
-        allCRServos.add(leftSweepOutServo);
-        allCRServos.add(rightSweepOutServo);
-
-        allServos.add(screwDoor);
-        // Don't we need the Touch Sensors here? =======================================================
-        
+        myOpMode.telemetry.addData(">", "Hardware Initialized");
+        myOpMode.telemetry.update();
     }
 
     /**
@@ -125,177 +57,56 @@ public class HardwareFile2024 {
      *
      * @param Drive     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
      * @param Turn      Right/Left turning power (-1.0 to 1.0) +ve is CW
-    */
+     */
      
-    double drive;
-    double strafe;
-    double turn;
+     double axial;
+     double strafe;
+     double rotation;
      
-    public void driveRobot(double drive, double strafe, double turn) {
-    // Combine drive and turn and strafe for blended motion.
-        double leftFrontPower  = - drive - strafe - turn;
-        double rightFrontPower = - drive + strafe + turn;
-        double leftBackPower   =   drive + strafe - turn;
-        double rightBackPower  =   drive - strafe + turn;
+    public void driveRobot(double axial, double strafe, double rotation) {
+        // Combine drive and turn for blended motion.
+            double leftFrontPower  = - axial - strafe - rotation;
+            double rightFrontPower = - axial + strafe + rotation;
+            double leftBackPower   =   axial + strafe - rotation;
+            double rightBackPower  =   axial - strafe + rotation;
         
-    // Scale the values so neither exceed +/- 1.0          
-        double max;
-        double min;
+        // Scale the values so neither exceed +/- 1.0          
+            double max;
+            double min;
         
-    // Normalize the values so no wheel power exceeds 100%
-    // This ensures that the robot maintains the desired motion.
-        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        max = Math.max(max, Math.abs(leftBackPower));
-        max = Math.max(max, Math.abs(rightBackPower));
-    // Do not comment or delete this part of the code!!! It fixed multiple issues we had meet 1
-        min = Math.min(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        min = Math.min(min, Math.abs(leftBackPower));
-        min = Math.min(min, Math.abs(rightBackPower));
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+            
+            min = Math.min(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            min = Math.min(min, Math.abs(leftBackPower));
+            min = Math.min(min, Math.abs(rightBackPower));
 
-        if (max > 1.0) {
-            leftFrontPower  /= max;
-            rightFrontPower /= max;
-            leftBackPower   /= max;
-            rightBackPower  /= max;
-        }
+            if (max > 1.0) {
+                leftFrontPower  /= max;
+                rightFrontPower /= max;
+                leftBackPower   /= max;
+                rightBackPower  /= max;
+            }
+            
+            if (min < -1.0) {
+              leftFrontPower    = -1.0;
+              rightFrontPower   = -1.0;
+              leftBackPower     = -1.0;
+              rightBackPower    = -1.0;
+            }
 
-        if (min < -1.0) {
-            leftFrontPower  /= min;
-            rightFrontPower /= min;
-            leftBackPower   /= min;
-            rightBackPower  /= min;
-
-    // Send calculated power to wheels
+        // Send calculated power to wheels
         leftFrontWheel.setPower(leftFrontPower);
         rightFrontWheel.setPower(rightFrontPower);
         leftBackWheel.setPower(leftBackPower);
         rightBackWheel.setPower(rightBackPower);
-/*
-        myOpMode.telemetry.addData("Status", "Run Time: " + runtime.toString());
-        myOpMode.telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-        myOpMode.telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-        myOpMode.telemetry.update();
-            */
-    }
-}
 
-    // public void activateSweeper() { 
-    //     highBrush.setPower(0.5 + BRUSH_SPEED);
-    //     lowBrush.setPower (0.5 - BRUSH_SPEED);
-    // }
-    // public void deactivateSweeper() { 
-    //     highBrush.setPower(0.5);
-    //     lowBrush.setPower (0.5);
-    // }
-
-    public void toggleFingers(String power) {
-        if (power == "on") {
-        highFinger.setPower(0.5 + BRUSH_SPEED);
-        lowFinger.setPower (0.5 - BRUSH_SPEED);
-        } else {
-        highFinger.setPower(0.5);
-        lowFinger.setPower (0.5);
-        }
+            myOpMode.telemetry.addData("Status", "Run Time: " + runtime.toString());
+            myOpMode.telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            myOpMode.telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            myOpMode.telemetry.update();
     }
-    public void armExpand() { 
-        if (!brushMoving){
-            if (brushExtended.isPressed()){
-                leftSweepOutServo.setPower  (0.5 + REACH_SPEED);
-                rightSweepOutServo.setPower (0.5 - REACH_SPEED);
-            } else {
-                leftSweepOutServo.setPower  (0.5 - REACH_SPEED);
-                rightSweepOutServo.setPower (0.5 + REACH_SPEED);
-            }
-            brushMoving = true;
-        }
-        // We need to write in the opmode: if 
-    }
-    public void standUp(String height) {
-        double waitTime = runtime.milliseconds();
-        // Need to get measurements for how much we need the feet
-        // to expand during each of these phases (combine like ones later)
-        if (height == "FullUp") {
-            // max expand is 7.5
-            leftFoot.setPower  (1.0);
-            rightFoot.setPower (1.0);
-            waitTime += (double)FULL_FEET_LIFT_SPEED;
-        } else if (height == "LowBasketUp") {
-            leftFoot.setPower  (1.0);
-            rightFoot.setPower (1.0);
-            waitTime += (double)LOW_FEET_LIFT_SPEED;
-        } else if (height == "RestFromLow") {
-            leftFoot.setPower  (-1.0);
-            rightFoot.setPower (-1.0);
-            waitTime += (double)LOW_FEET_LIFT_SPEED;
-        } else if (height == "RestFromFull") {
-            leftFoot.setPower  (-1.0);
-            rightFoot.setPower (-1.0);
-            waitTime += (double)FULL_FEET_LIFT_SPEED;
-        }
-        while (runtime.milliseconds() <= waitTime) {
-        }
-    }
-    
-    public void liftScrew(String height) {
-        // We need measurements for how high we need the screw to go
-        // during each of these scenarios (combine like heights later)
-        if (height == "FullUp") {
-            // max expand is 7.5
-            leftFoot.setPower  (1.0);
-            rightFoot.setPower (1.0);
-            // sleep(FULL_SCREW_LIFT_SPEED);
-        } else if (height == "LowBasketUp") {
-            leftFoot.setPower  (1.0);
-            rightFoot.setPower (1.0);
-            // sleep(LOW_SCREW_LIFT_SPEED);
-        } else if (height == "RestFromLow") {
-            leftFoot.setPower  (-1.0);
-            rightFoot.setPower (-1.0);
-            // sleep(LOW_SCREW_LIFT_SPEED);
-        } else if (height == "RestFromFull") {
-            leftFoot.setPower  (-1.0);
-            rightFoot.setPower (-1.0);
-            // sleep(FULL_SCREW_LIFT_SPEED);
-    }
-}
-    public void screwTurnPower(double spin) {
-        screwTurn.setPower(spin);
-    }
-    
-
-    public void toggleDepositDoor(String power) {
-        if (power == "on") {
-            screwDoor.setPosition(180); //untested
-        } else {
-            screwDoor.setPosition(0); // untested
-        }
-    }
-    
-    public ArrayList telemetryUpdate() {
-        ArrayList<String[]> returnVal = new ArrayList<>();
-        for (DcMotor thisMotor: allMotors) {
-            String[] push = {"MotorSpeed", Double.toString(thisMotor.getPower())};
-            returnVal.add(push);
-        }
-        for (CRServo thisServo: allCRServos) {
-            String[] push = {"ServoPower", Double.toString(thisServo.getPower())};
-            returnVal.add(push);
-        }
-        for (Servo   thisServo: allServos) {
-            String[] push = {"ServoPosition", Double.toString(thisServo.getPosition())};
-            returnVal.add(push);
-        }
-        return returnVal;
-    }
-
-    //public void staticvar(String wantedVar) {
-    //    if (wantedVar == "PANEL") {
-    //        return PANEL;
-    //    } else if (wantedVar == "INCHES") {
-    //        return INCH;
-    //    } else {
-    //        telemetry.addData("Couldn't get that variable's data from the hardware");
-    //        telemetry.update();
-    //    }
-    //}  
 }
