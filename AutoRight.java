405d8cc52bf44e6843bc436699434d67fd70e623
Devilish-Taco <santiagomoyle@gmail.com>
2025-01-07 15:02:31 -0800

    Update AutoRight.java

--- AutoRight.java
+++ AutoRight.java
@@ -1,1 +1,18 @@
+import org.firstinspires.ftc.teamcode.HardwareFile2024;
+import org.firstinspires.ftc.teamcode.AutonomousHardware;
+@Autonomous(name="RightSideBasicAutonomous", group="9044.NERD.")
+public class Autonomous extends LinearOpMode {
 
+    @Override
+    public void runOpMode() {
+      
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+          
+        }
+    }
+}
