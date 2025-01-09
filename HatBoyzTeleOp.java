b78fc158645c6125be426bcacd294ace6c970146
Mangyfire <130502498+Mangyfire@users.noreply.github.com>
2025-01-09 15:27:19 -0800

    Update HatBoyzTeleOp.java

--- HatBoyzTeleOp.java
+++ HatBoyzTeleOp.java
@@ -25,6 +25,29 @@ public class HatBoyzTeleOp extends LinearOpMode {
     while (opModeIsActive()) {
 
       
+      //movement controller
+      driveRobot(gamepad1.left_stick_y,gamepad1.left_stick_x,gamepad1.right_stick_x);
+      //(axial,strafe,rotation)
+
+      //Finger controll
+      if (gamepad2.right_bumper) {
+        toggleFingers("on");
+      }
+      if (gamepad2.left_bumper) {
+        toggleFingers("off");
+      }
+
+      //standing code
+      standUp(Math.abs(gamepad2.right_stick_y)/gamepad2.right_stick_y);
+
+      //arm reach code
+      if (gamepad2.a){
+        armExpand();
+      }
+
+      //screw lift
+      liftScrew(Math.abs(gamepad2.left_stick_y)/gamepad2.left_stick_y);
+      
     }
   }
 }
