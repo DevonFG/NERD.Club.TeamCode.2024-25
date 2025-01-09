ac1cb9210a1097011044bf01b2aebcec29333091
derekz09 <derek@thezakarians.com>
2025-01-09 14:43:43 -0800

    Update AutoLeft.java
    Finish code and wrote a lot of ideas and other comments

--- AutoLeft.java
+++ AutoLeft.java
@@ -35,14 +35,28 @@ public class AutoWithHardware extends LinearOpMode {
         // Wait for the game to start (driver presses START)
         waitForStart();
 
+        /*
+            Plan: 
+            1: Start with a sample and move to baskets           Done
+            2: Stand up to high basket and release sample        Done
+            3: Close Deposit door and lower to the ground        Done
+            4: Turn and drive to grab a neutral sample
+            5: Move to Ascent Zone and touch the bar for a level one ascent
+
+
+
+        */
+
+
+        
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
+        
             robot.spinScrew(1.0); // Whatever full speed is
             sleep(100);
             
             robot.driveRobot(0.0, 1.0, 0.0); // (drive, strafe, turn) Get away from wall
-            sleep(2*INCH_TO_TICK);
+            sleep(6*INCH_TO_TICK);
             driveRobot(0.0, 0.0, 0.0); 
             sleep(100);
                 
@@ -65,8 +79,36 @@ public class AutoWithHardware extends LinearOpMode {
             robot.liftScrew("RestFromFull");  
             sleep(100);
 
-            robot.driveRobot(0.0, 0.0, 0.0) // Will be spinning toward the observation zone
-            sleep()
+            robot.driveRobot(0.0, 0.0, -1.0); // Spin toward the neutral sample closest to the center
+            sleep(100 *  DEGREE_TO_TICK); // Change 100 to the correct degrees after testing
+            robot.driveRobot(0.0, 0.0, 0.0);
+            sleep(100);
+
+            robot.spinScrew(1.0); //Start to spin the screw to pick up the sample
+
+            robot.driveRobot(1.0, 0.0, 0.0);
+            sleep(33*INCH_TO_TICK);  // Untested value drive towards sample and past to be next to ascent zone
+            robot.driveRobot(0.0, 0.0, 0.0);
+            sleep(100);
+
+            robot.driveRobot(0.0, 0.0, 1.0); // Turn to be facing ascent zone
+            sleep(80 * DEGREE_TO_TICK);
+            robot.driveRobot(0.0, 0.0, 0.0);
+            sleep(100);
+
+            robot.driveRobot(1.0, 0.0, 0.0);
+            sleep(24*INCH_TO_TICK);  // Untested value drive to ascent zone wall
+            robot.driveRobot(0.0, 0.0, 0.0);
+            sleep(100);
+
+            robot.spinScrew(0.0); //Stop the screw from spinning and hope that the sample stays at the top
+            // The next line is to touch the 1st ascent zone bar, but that hasn't been fully planned on the robot, so it could be scrapped.
+            // The robot may also be able to hit the bar without standing up, but idk
+            robot.standUp("FullUp"); 
+
+
+            
+            
         }
     }
 }
